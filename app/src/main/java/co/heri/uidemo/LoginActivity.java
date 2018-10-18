207b90094e6b3f68c6c0488c8c472a077af7aa6d
Heri Agape <agape@live.fr>
2018-10-18 19:41:45 +0300

    added button to go the MainActivity

--- app/src/main/java/co/heri/uidemo/LoginActivity.java
+++ app/src/main/java/co/heri/uidemo/LoginActivity.java
@@ -2,7 +2,13 @@ package co.heri.uidemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        MaterialButton mapBTN = findViewById(R.id.mapBTN);
+
+        mapBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Snackbar.make(v, "Proceed to login?", Snackbar.LENGTH_LONG).setAction("Proceed", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
+                        myIntent.putExtra("name", "Heri Agape");
+                        startActivity(myIntent);
+                    }
+                }).show();
+
+
+            }
+        });
     }
 }

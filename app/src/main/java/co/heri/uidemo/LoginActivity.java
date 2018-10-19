1480aa3ffb27e3ef604389c84ee8a7c7db314bc6
Heri Agape <agape@live.fr>
2018-10-19 21:48:04 +0300

    added login latout and design

--- app/src/main/java/co/heri/uidemo/LoginActivity.java
+++ app/src/main/java/co/heri/uidemo/LoginActivity.java
@@ -22,19 +22,10 @@ import android.widget.TextView;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.snackbar.Snackbar;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-
 public class LoginActivity extends AppCompatActivity {
 
     SocketComm mySocket;
 
-    User[] myUsers = {
-            new User("Peter Kamau", 19, "agape@live.fr", "25/05/2001", "Male", "0700928129"),
-            new User("Joanna Terry", 20, "peter@live.fr", "25/05/1997", "Female", "0700928128"),
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,35 +39,6 @@ public class LoginActivity extends AppCompatActivity {
         mySocket = new SocketComm(this,"https://ui-demo.eu-gb.mybluemix.net/", online_status);
 
         mySocket.connect();
-
-
-        ArrayAdapter<User> usersAdapter =
-                new ArrayAdapter<User>(this, 0, myUsers) {
-                    @Override
-                    public View getView(int position,
-                                        View convertView,
-                                        ViewGroup parent) {
-
-                        User currentUser = myUsers[position];
-
-                        if(convertView == null) {
-                            convertView = getLayoutInflater()
-                                    .inflate(R.layout.user_list, null, false);
-                        }
-
-                        TextView user_name = convertView.findViewById(R.id.user_name);
-                        TextView user_email = convertView.findViewById(R.id.user_email);
-
-                        user_name.setText(currentUser.getName());
-                        user_email.setText(currentUser.getEmail());
-
-                        return convertView;
-
-                    }
-                };
-
-        ListView list_of_users = findViewById(R.id.list_of_users);
-        list_of_users.setAdapter(usersAdapter);
 
 
         mapBTN.setOnClickListener(new View.OnClickListener() {
